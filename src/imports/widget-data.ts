654e6cda84362f644ea03ca78a4dbf07ea985fd6
starkwolf88 <mattsmith191088@gmail.com>
2026-01-21 20:51:43 +0000

    Update widget-data.ts

--- src/imports/widget-data.ts
+++ src/imports/widget-data.ts
@@ -10,5 +10,49 @@ export const widgetData = {
                 }
             }
         }
+    },
+    farming: {
+        tool_leprechaun: {
+            deposit: {
+                spade: {
+                    packed_widget_id: 8257539,
+                    identifier: 2,
+                    opcode: 57,
+                    p0: -1
+                },
+                secateurs: {
+                    packed_widget_id: 8257540,
+                    identifier: 2,
+                    opcode: 57,
+                    p0: -1
+                },
+                bottomlessBucket: {
+                    packed_widget_id: 8257544,
+                    identifier: 1,
+                    opcode: 57,
+                    p0: -1
+                }
+            },
+            withdraw: {
+                spade: {
+                    packed_widget_id: 8192010,
+                    identifier: 1,
+                    opcode: 57,
+                    p0: -1
+                },
+                secateurs: {
+                    packed_widget_id: 8192011,
+                    identifier: 1,
+                    opcode: 57,
+                    p0: -1
+                },
+                bottomlessBucket: {
+                    packed_widget_id: 8192015,
+                    identifier: 1,
+                    opcode: 57,
+                    p0: -1
+                }
+            }
+        }
     }
 };
