9808f1f0c2ca8544fc6536f5f3d87fcbb7a690c5
starkwolf88 <mattsmith191088@gmail.com>
2026-01-21 23:10:57 +0000

    Update widget-data.ts

--- src/imports/widget-data.ts
+++ src/imports/widget-data.ts
@@ -14,6 +14,18 @@ export const widgetData = {
     farming: {
         tool_leprechaun: {
             deposit: {
+                rake: {
+                    packed_widget_id: 8257537,
+                    identifier: 2,
+                    opcode: 57,
+                    p0: -1
+                },
+                seed_dibbler: {
+                    packed_widget_id: 8257538,
+                    identifier: 2,
+                    opcode: 57,
+                    p0: -1
+                },
                 spade: {
                     packed_widget_id: 8257539,
                     identifier: 2,
@@ -34,6 +46,18 @@ export const widgetData = {
                 }
             },
             withdraw: {
+                rake: {
+                    packed_widget_id: 8192008,
+                    identifier: 2,
+                    opcode: 57,
+                    p0: -1
+                },
+                seed_dibbler: {
+                    packed_widget_id: 8192009,
+                    identifier: 2,
+                    opcode: 57,
+                    p0: -1
+                },
                 spade: {
                     packed_widget_id: 8192010,
                     identifier: 1,
